2d4950e04018892b460a6d893d1793e430f545c3
Joel Angel <joelbaraka4325@gmail.com>
2022-05-27 11:59:49 +0300

    separated the card component and rendered it to the App.js

---
+++ components/CarItem/index.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import React from "react";
+import styles from "./styles";
+
+const CarItem = () => {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground
+        source={require("../../assets/images/ModelX.jpeg")}
+        style={styles.image}
+      />
+
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subTitle}>Starting at $70,000</Text>
+      </View>
+    </View>
+  );
+};
+
+export default CarItem;
+
+//rnfce
